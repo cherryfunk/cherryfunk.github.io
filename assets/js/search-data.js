059e96aeb6599415878d77077887f84428e8db97
cherryfunk <74245948+cherryfunk@users.noreply.github.com>
2025-11-16 14:23:49 +0000

    Deploying to gh-pages from @ cherryfunk/cherryfunk.github.io@369bdc087a6698f9cc70896c8b08487ca9c95c15 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-on-muller-a-modular-monad-based-semantics-of-the-neurosymbolic-uller-framework-is-accepted-at-nesy-2025-this-was-my-main-project-before-starting-my-phd",
+          title: 'Our paper on “mULLER: A Modular Monad-Based Semantics of the Neurosymbolic ULLER Framework”...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
